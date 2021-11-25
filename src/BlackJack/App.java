b55727f7a4606479d7805af5ee29c45a919736e9
emmettlaw <emmettlaw10@gmail.com>
2021-11-25 06:42:25 -0500

    initial upload

---
+++ src/BlackJack/App.java
@@ -0,0 +1,7 @@
+package BlackJack;
+
+public class App {
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
---
+++ src/BlackJack/Dealer.java
@@ -0,0 +1,11 @@
+/**
+ * this class models the dealer
+ *
+ * @author Emmett Law Aug 2021
+ */
+package BlackJack;
+
+public class Dealer {
+    Hand dealerHand = new Hand();
+
+}
